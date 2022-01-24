d9ad6e336d0468eca4abe460c42fa1fadf03b071
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2022-01-24 07:04:12 +0100

    setup of eslint and prettier

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, StatusBar, Text, View, SafeAreaView } from 'react-native';
+import {
+  DefaultTheme,
+  Searchbar,
+  useTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  dark: true,
+  mode: 'exact',
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+};
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const { colors } = useTheme();
+  const onChangeSearch = query => setSearchQuery(query);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar
+            placeholder="Search "
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+        </View>
+        <View style={[styles.list, { borderColor: colors.primary }]}>
+          <Text theme={{ fonts: { medium: 'Open Sans' } }}>{searchQuery}</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  list: {
+    flex: 1,
+    borderWidth: 2,
+    padding: 16,
   },
 });
